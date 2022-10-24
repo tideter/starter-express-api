8d59d1de0ed726b06e380ed194874c626fd4af48
tide <65201531+tideter@users.noreply.github.com>
2022-10-25 00:56:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.all('/test', (req, res) => {
+    console.log("Just got a request!")
+    res.send('This is a test endpoint')
+})
+
 app.listen(process.env.PORT || 3000)
